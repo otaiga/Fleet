4b5ef2877aa06a9cc733eca45bff7798415e8c55
Karl Turner <karl.turner5@gmail.com>
2012-02-16 12:43:02 +0000

    .

--- db/migrate/20120216105437_removenullfrompolicy.rb
+++
@@ -1,9 +0,0 @@
-class Removenullfrompolicy < ActiveRecord::Migration
-  def up
-  	remove_column :policies, :group_id, :integer, :null => false
-  end
-
-  def down
-  	add_column :policies, :group_id, :integer, :null => false
-  end
-end
--- db/migrate/20120216110109_removenullfrompolicy3.rb
+++
@@ -1,9 +0,0 @@
-class Removenullfrompolicy3 < ActiveRecord::Migration
-  def up
-    add_column :policies, :group_id, :integer
-  end
-
-  def down
-  	remove_column :policies, :group_id, :integer
-  end
-end
