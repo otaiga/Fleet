b0f6d25c665ec3b0f1319d94004d466e2713f613
Karl Turner <karl.turner5@gmail.com>
2012-02-16 12:42:38 +0000

    .

--- db/migrate/20120216103913_addfieldstopolicy.rb
+++ db/migrate/20120216103913_addfieldstopolicy.rb
@@ -2,12 +2,12 @@ class Addfieldstopolicy < ActiveRecord::Migration
   def up
   add_column :policies, :name, :string
 	add_column :policies, :account_id, :integer, :null => false
-	add_column :policies, :group_id, :integer, :null => false
+	add_column :policies, :group_id, :integer
   end
 
   def down
   remove_column :policies, :name, :string
 	remove_column :policies, :account_id, :integer, :null => false
-	remove_column :policies, :group_id, :integer, :null => false
+	remove_column :policies, :group_id, :integer
   end
 end
