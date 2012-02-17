1e0df7ae18401db2d599c9267709d564fd62ad89
Karl Turner <karl.turner5@gmail.com>
2012-02-17 14:16:36 +0000

    .

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,9 +2,9 @@ class ApiController < ApplicationController
 before_filter :http_basic_authentication
 
   def index
-    @devices = Group.all
+    @policy = Policy.all
     respond_to do |format|
-      format.xml { render :xml => @devices.to_xml }
+      format.xml { render :xml => @policy.to_xml }
       format.html { redirect_to root_path }
     end
   end
@@ -12,8 +12,8 @@ before_filter :http_basic_authentication
 
   protected
     def http_basic_authentication
-      @username = Group.first.name
-      @password = Group.last.name
+      @username = Group.where(:account_id => 1).first.name
+      @password = Group.where(:account_id => 1).last.name
       if request.format == Mime::XML
         authenticate_or_request_with_http_basic do |username, password|
         username == @username && password == @password
